0d84069549f32d6edf6c8430b8cd869081ea5232
unfair <2814315943@qq.com>
2024-07-23 17:17:40 +0800

    ready to submit my lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,10 +295,14 @@ fork(void)
 
   np->state = RUNNABLE;
 
+// 子进程复制父进程的 mask 
+  np->mask = p->mask;
+
   release(&np->lock);
 
   return pid;
 }
+
 
 // Pass p's abandoned children to init.
 // Caller must hold p->lock.
@@ -692,4 +696,28 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
 }
